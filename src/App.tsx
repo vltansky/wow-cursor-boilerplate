31581459858e0d8a3e3ac564862aa8daf3dbd358
Vlad <vl.tansky@gmail.com>
2025-04-30 13:48:01 +0300

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import wowLogo from "/wow.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="flex flex-col items-center justify-center h-screen bg-black/90">
+      <img src={wowLogo} className="w-64 h-64" alt="React logo" />
       <button
         onClick={() => setCount((count) => count + 1)}
-        className="rounded-md bg-primary mt-5 px-4 py-2 text-primary-foreground hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+        className="rounded-md bg-white text-primary  mt-5 px-4 py-2 hover:bg-white/60 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
       >
         count is {count}
       </button>
